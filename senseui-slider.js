1cf66dbbe82bba98c53dcc51491277ea534e137b
Yianni Ververis <yiannisver@gmail.com>
2017-03-27 13:28:01 -0400

    v1.1

--- senseui-slider.js
+++ senseui-slider.js
@@ -8,7 +8,6 @@
  * @example https://github.com/yianni-ververis/SenseUI-Slider
  */
 
-// text.js has cross-domain restrictions
 define([
 	"qlik",
 	"jquery",
@@ -148,6 +147,12 @@ define([
 									ref: 'vars.ordinal',
 									defaultValue: false
 							    },
+								sliderSingle: {
+									type: "boolean",
+									label: 'Single Slider',
+									ref: 'vars.sliderSingle',
+									defaultValue: false
+							    },
 								SliderInputWidth: {
 									type: "number",
 									expression: "none",
@@ -174,6 +179,12 @@ define([
 									ref: 'vars.font.color',
 									defaultValue: '#000'
 							    },
+								FontColorSelected: {
+									type: "string",
+									label: 'Selected Color',
+									ref: 'vars.font.selected',
+									defaultValue: '#FF7A00'
+							    },
 							}
 						}
 					}
@@ -188,6 +199,7 @@ define([
 	// Alter properties on edit		
 	me.paint = function($element,layout) {
 		var vars = {
+			v: "1.1",
 			id: layout.qInfo.qId,
 			field: layout.qListObject.qDimensionInfo.qFallbackTitle,
 			object: layout.qListObject.qDataPages[0].qMatrix,
@@ -204,6 +216,7 @@ define([
 			},
 			padding: (layout.vars.padding) ? layout.vars.padding : 5,
 			ordinal: (layout.vars.ordinal) ? true : false,
+			sliderSingle: (layout.vars.sliderSingle) ? true : false,
 			template: '',
 			input: {
 				width: (layout.vars.input.width) ? layout.vars.input.width : 4,
@@ -211,12 +224,12 @@ define([
 			font: {
 				size: (layout.vars.font.size) ? layout.vars.font.size : 12,
 				color: (layout.vars.font.color) ? layout.vars.font.color : '#000',
+				selected: (layout.vars.font.selected) ? layout.vars.font.selected : '#FF7A00',
 			},
 			height: $element.height(),
 			width: $element.width(),
 			this: this,		
 		}
-
 
 		if (typeof layout.vars.range === 'undefined') {
 			layout.vars.range = {
@@ -232,27 +245,24 @@ define([
 		layout.vars.range.minDis = (vars.ordinal) ? me.getGetOrdinal(layout.vars.range.min) : layout.vars.range.min;
 		layout.vars.range.maxDis = (vars.ordinal) ? me.getGetOrdinal(layout.vars.range.max) : layout.vars.range.max;
 
+		console.log(vars)
+		
 		//Get Selection Bar
 		me.app.getList("SelectionObject", function(reply){
 			var selectedFields = reply.qSelectionObject.qSelections;
 			if (_.where(selectedFields, {'qField': vars.field}) && _.where(selectedFields, {'qField': vars.field}).length) {
-				var selectedObject = _.filter(vars.object, function(obj){ return obj[0].qState === 'S'; });
-				// if (selectedObject.length >= 1) {
-				// 	var min = _.min(selectedObject, function(o){return o[0].qNum;})[0].qNum,
-				// 		max = _.max(selectedObject, function(o){return o[0].qNum;})[0].qNum;
-				// 	layout.vars.range.values = [min, max];
-				// 	$("#sliderBar").slider( "option", "values", layout.vars.range.values );
-				// } else {	
-					$( "#" + vars.id + "_slider #input_from" ).val(layout.vars.range.values[0]);
-					$( "#" + vars.id + "_slider #input_to" ).val(layout.vars.range.values[1]);
-					$("#sliderBar").slider( "option", "values", layout.vars.range.values);
-				// }
-			} else {	
-				layout.vars.range.values[0] = layout.vars.range.min;
-				layout.vars.range.values[1] = layout.vars.range.max;
-				$( "#" + vars.id + "_slider #input_from" ).val(layout.vars.range.values[0]);
-				$( "#" + vars.id + "_slider #input_to" ).val(layout.vars.range.values[1]);
-				$("#sliderBar").slider( "option", "values", layout.vars.range.values);
+				var selectedObject = _.filter(vars.object, function(obj){ 
+					return obj[0].qState === 'S'; 
+				});
+				if (selectedObject.length >= 1) {
+					var min = _.min(selectedObject, function(o){return o[0].qNum;})[0].qNum,
+						max = _.max(selectedObject, function(o){return o[0].qNum;})[0].qNum;
+					layout.vars.range.values = [min, max];
+					$("#sliderBar").slider( "option", "values", layout.vars.range.values );
+					
+					$(`#${vars.id}_slider #input_from`).val(min)
+					$(`#${vars.id}_slider #input_to`).val(max)
+				}
 			}
 		});
 
@@ -288,7 +298,7 @@ define([
 	    // me.drawSlider = function () {
 	    if ($('#sliderBar').is(':empty')){
 			$( "#sliderBar" ).slider({
-				range: true,
+				range: (!vars.sliderSingle)?true:false,
 				min: layout.vars.range.min,
 				max: layout.vars.range.max,
 				values: layout.vars.range.values,
@@ -332,9 +342,25 @@ define([
 			$(this).css("background",vars.handleColor);
 		});
 
+		// Hover colors for all labels except the 2 selected
+		$("#" + vars.id + "_slider .ui-slider-pip:not(.ui-slider-pip-selected-1):not(.ui-slider-pip-selected-2)").hover(function(){
+			$(this).css("color", vars.font.selected);
+		},function(){
+			$(this).css("color", vars.font.color);
+		});
+		// Hover colors for selected labels
+		$("#" + vars.id + "_slider .ui-slider-pip-selected-1, #" + vars.id + "_slider .ui-slider-pip-selected-2").hover(function(){
+			$(this).css("color", vars.font.selected);
+		},function(){
+			$(this).css("color", vars.font.selected);
+		});
+
 		$("#" + vars.id + "_slider").css("font-size", vars.font.size);
-		$("#" + vars.id + "_slider .ui-slider-pips .ui-slider-pip").css("font-size", vars.font.size);
+		$("#" + vars.id + "_slider .ui-slider-pip").css("font-size", vars.font.size);
+		$("#" + vars.id + "_slider .ui-slider-pip").css("color", vars.font.color);
 		$("#" + vars.id + "_slider").css("color", vars.font.color);
+		$("#" + vars.id + "_slider .ui-slider-pip-selected-1").css("color", vars.font.selected);
+		$("#" + vars.id + "_slider .ui-slider-pip-selected-2").css("color", vars.font.selected);
 		$("#" + vars.id + "_slider #sliderTop ").css("padding-bottom", vars.padding);
 		
 	    me.selectRange = function () {	
@@ -360,12 +386,12 @@ define([
 // 		    	$("#sliderBar").slider( 'values', layout.vars.range.values );
 // 			})
 			// This is only for a field selection not a dimension. I have to make the app.field work all of the time 
-			vars.this.backendApi.selectValues(0, rangeSelected2, false).then(function(e){
-				$( "#" + vars.id + "_slider #input_from" ).val(layout.vars.range.values[0]);
-				$( "#" + vars.id + "_slider #input_to" ).val(layout.vars.range.values[1]);
-		    	$("#sliderBar").slider( 'values', layout.vars.range.values );
-			})
+			$( "#" + vars.id + "_slider #input_from" ).val(layout.vars.range.values[0]);
+			$( "#" + vars.id + "_slider #input_to" ).val(layout.vars.range.values[1]);
+	    	$("#sliderBar").slider( 'values', layout.vars.range.values );
+			vars.this.backendApi.selectValues(0, rangeSelected2, false)
 	    }
+		console.info('%c SenseUI-Slider ' + vars.v + ': ', 'color: red', '#' + vars.id + ' Loaded!');
 	};
 
 	// define HTML template
@@ -373,6 +399,8 @@ define([
 
 	// Controller for binding
 	me.controller =['$scope','$rootScope', function($scope,$rootScope){
+		$scope.$watchCollection('layout.vars.range', function(obj) {
+		})
 	}];
 
 	// Return Ordinal Numbers 1st, 2nd etc.
